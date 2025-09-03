c37b3f93bee8d92df0f57f3fc16be2bd9e778711
Haochen Wu <wuhaochen42@gmail.com>
2025-09-03 10:26:54 +0800

    define chain and link

--- internal/ssc/ssc.go
+++ internal/ssc/ssc.go
@@ -1,2 +1,20 @@
 // Supersession chain
 package ssc
+
+import (
+	"github.com/google/uuid"
+
+	"github.com/tychonis/cyanotype/model"
+)
+
+type ChainID = uuid.UUID
+
+type Chain struct {
+	ID   ChainID
+	Head model.ItemID
+}
+
+type Link struct {
+	Item  model.ItemID
+	Chain ChainID
+}
