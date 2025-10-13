83e56015a9da3bd9aa99ce7b4d556a6882f58b3b
Haochen Wu <wuhaochen42@gmail.com>
2025-10-13 09:40:51 +0800

    refactor

--- internal/parser/hcl/block_parser.go
+++ internal/parser/hcl/block_parser.go
@@ -83,6 +83,53 @@ func (c *Core) buildCompanionCoProcess(item *model.Item, coItem *model.CoItem) (
 	return cp, c.Catalog.Add(cp)
 }
 
+func (c *Core) processSyntaxSugar(ctx *ParserContext, from []*UnresolvedBOMLine) ([]*model.BOMLine, error) {
+	if len(from) <= 0 {
+		return nil, nil
+	}
+	ret := make([]*model.BOMLine, 0, len(from))
+	for _, comp := range from {
+		qualifier := refToQualifier(ctx, comp.Ref)
+		compItemSym, err := c.Catalog.Find(qualifier)
+		if err != nil {
+			if err != catalog.ErrNotFound {
+				return nil, err
+			} else {
+				sym, err := c.Resolve(ctx, comp.Ref)
+				if err != nil {
+					return nil, err
+				}
+				unprocessed, ok := sym.(*UnprocessedSymbol)
+				if !ok {
+					return nil, errors.New("wrong symbol type")
+				}
+				compItemSym, err = c.ParseSymbol(unprocessed)
+				if err != nil {
+					return nil, err
+				}
+			}
+		}
+
+		compItem, ok := compItemSym.(*model.Item)
+		if !ok {
+			return nil, errors.New("incorrect ref")
+		}
+		compCoItems, err := c.Catalog.GetCoItems(compItem.Digest)
+		if err != nil {
+			return nil, err
+		}
+		if len(compCoItems) != 1 {
+			slog.Debug("error", "item", compItem.Qualifier, "length", len(compCoItems), "digest", compItem.Digest)
+			return nil, errors.New("not implemented yet")
+		}
+		ret = append(ret, &model.BOMLine{
+			Item: compCoItems[0].Item,
+			Qty:  comp.Qty,
+		})
+	}
+	return ret, nil
+}
+
 func (c *Core) buildCompanionForItem(ctx *ParserContext, item *model.Item, from []*UnresolvedBOMLine) error {
 	slog.Debug("build companions", "module", ctx.CurrentModule(), "item", item.Qualifier)
 
@@ -99,46 +146,11 @@ func (c *Core) buildCompanionForItem(ctx *ParserContext, item *model.Item, from 
 	if len(from) <= 0 {
 		return nil
 	}
-	input := make([]*model.BOMLine, 0, len(from))
-	for _, comp := range from {
-		qualifier := refToQualifier(ctx, comp.Ref)
-		compItemSym, err := c.Catalog.Find(qualifier)
-		if err != nil {
-			if err != catalog.ErrNotFound {
-				return err
-			} else {
-				sym, err := c.Resolve(ctx, comp.Ref)
-				if err != nil {
-					return err
-				}
-				unprocessed, ok := sym.(*UnprocessedSymbol)
-				if !ok {
-					return errors.New("wrong symbol type")
-				}
-				compItemSym, err = c.ParseSymbol(unprocessed)
-				if err != nil {
-					return err
-				}
-			}
-		}
-
-		compItem, ok := compItemSym.(*model.Item)
-		if !ok {
-			return errors.New("incorrect ref")
-		}
-		compCoItems, err := c.Catalog.GetCoItems(compItem.Digest)
-		if err != nil {
-			return err
-		}
-		if len(compCoItems) != 1 {
-			slog.Debug("error", "item", compItem.Qualifier, "length", len(compCoItems), "digest", compItem.Digest)
-			return errors.New("not implemented yet")
-		}
-		input = append(input, &model.BOMLine{
-			Item: compCoItems[0].Item,
-			Qty:  comp.Qty,
-		})
+	input, err := c.processSyntaxSugar(ctx, from)
+	if err != nil {
+		return err
 	}
+
 	p := &model.Process{
 		Qualifier: getImplicitProcessQualifier(item),
 		Output: []*model.BOMLine{
