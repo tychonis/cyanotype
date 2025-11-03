b2ef04b508eeae0dde340869b0b547b64f9ca23a
Haochen Wu <wuhaochen42@gmail.com>
2025-11-03 15:17:44 +0800

    api catalog

--- core/parser/hcl/adhoc.go
+++ core/parser/hcl/adhoc.go
@@ -1,5 +1,20 @@
 package hcl
 
+import (
+	"github.com/tychonis/cyanotype/internal/catalog"
+	"github.com/tychonis/cyanotype/internal/ranker"
+	"github.com/tychonis/cyanotype/internal/symbols"
+)
+
+func NewCoreFromAPI(endpoint string) *Core {
+	return &Core{
+		Symbols: symbols.NewSymbolTable(),
+		Catalog: catalog.NewRemoteCatalog(endpoint),
+
+		Ranker: &ranker.NaiveRanker{},
+	}
+}
+
 // Adhoc function supporting bomhub.
 func (c *Core) SaveCatalog(endpoint string) error {
 	return c.Catalog.Save(endpoint)
