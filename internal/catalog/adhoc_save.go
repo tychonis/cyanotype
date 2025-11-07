e2dbc67f333a85b35f253b500c852f5f90a99534
Haochen Wu <wuhaochen42@gmail.com>
2025-11-07 14:27:56 +0800

    also save metadata

--- internal/catalog/adhoc_save.go
+++ internal/catalog/adhoc_save.go
@@ -1,19 +1,58 @@
 package catalog
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"net/http"
 
 	"github.com/tychonis/cyanotype/internal/serializer"
 )
+
+type CatalogMetadata struct {
+	Name        string `json:"string"`
+	UniqueParts int    `json:"unique_parts"`
+}
+
+func (c *Catalog) SaveMetadata(endpoint string, tag string) error {
+	metaDataEndpoint := fmt.Sprintf("%s/workspace/%s", endpoint, tag)
+	symbols, err := c.index.ListSymbols()
+	if err != nil {
+		return err
+	}
+	metadata := CatalogMetadata{
+		Name:        "placeholder",
+		UniqueParts: len(symbols),
+	}
+	content, err := json.Marshal(metadata)
+	if err != nil {
+		return err
+	}
+	resp, err := http.Post(metaDataEndpoint, "application/json", bytes.NewReader(content))
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusAccepted {
+		return errors.New("error response")
+	}
+	return nil
+}
 
 func (c *Catalog) Save(endpoint string, tag string) error {
 	localIndex, ok := c.index.(*LocalIndex)
 	if !ok {
 		return errors.New("can only save local index now")
 	}
+
+	err := c.SaveMetadata(endpoint, tag)
+	if err != nil {
+		return err
+	}
+
 	remote := RemoteIndexFromLocal(localIndex)
 	remote.Endpoint = endpoint + "/bom_index/" + tag
-	err := remote.Save()
+	err = remote.Save()
 	if err != nil {
 		return err
 	}
