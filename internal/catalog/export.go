3f7c272d50e28991a66ef5374ea86c2e855a9659
Haochen Wu <wuhaochen42@gmail.com>
2026-03-29 17:05:15 +0800

    export everything

--- internal/catalog/export.go
+++ internal/catalog/export.go
@@ -2,42 +2,28 @@ package catalog
 
 import (
 	"encoding/json"
-	"errors"
 
 	"github.com/tychonis/cyanotype/model"
 )
 
-type ItemInfo struct {
-	Name string `json:"name"`
-}
-
 type CatalogDocument struct {
-	Items map[model.Digest]*ItemInfo `json:"items"`
+	Symbols map[model.Digest]model.ConcreteSymbol `json:"symbols"`
 }
 
 func (c *Catalog) Export() ([]byte, error) {
 	doc := &CatalogDocument{
-		Items: make(map[model.Digest]*ItemInfo),
+		Symbols: make(map[model.Digest]model.ConcreteSymbol),
 	}
 	symbols, err := c.index.ListSymbols()
 	if err != nil {
 		return nil, err
 	}
-	for symDigest, symType := range symbols {
-		if symType == "item" {
-			sym, err := c.Get(symDigest)
-			if err != nil {
-				return nil, err
-			}
-			item, ok := sym.(*model.Item)
-			if !ok {
-				return nil, errors.New("unexpected type")
-			}
-			info := &ItemInfo{
-				Name: item.Content.Name,
-			}
-			doc.Items[symDigest] = info
+	for symDigest := range symbols {
+		sym, err := c.Get(symDigest)
+		if err != nil {
+			return nil, err
 		}
+		doc.Symbols[symDigest] = sym
 	}
 	return json.Marshal(doc)
 }
